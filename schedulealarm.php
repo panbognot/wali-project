7539bc87937f8d13ad2999c0d1dadde10ea23ba8
panbognot <prado.bognot@gmail.com>
2015-06-27 10:52:35 +0800

    - Modified the sql code for arranging the schedule based on "day of week" and "activate time". Sql is segregated by "cluster ids"

--- schedulealarm.php
+++ schedulealarm.php
@@ -347,9 +347,25 @@ include './header.php';
 				mysql_free_result($result);
 				*/
 
-				$sql="SELECT * FROM alarm_schedule WHERE clusterid=".$_GET['clusterid']." ORDER BY scheduleid ASC";
+				//$sql="SELECT * FROM alarm_schedule WHERE clusterid=".$_GET['clusterid']." ORDER BY scheduleid ASC";
+				$sql="SELECT 
+						alarm_schedule.scheduleid, 
+						alarm_schedule.clusterid, 
+						alarm_schedule.activate_time, 
+						alarm_schedule.brightness, 
+						day_of_week.day AS day_of_week, 
+						day_of_week.dow_id 
+					FROM 
+						alarm_schedule 
+					INNER JOIN 
+						day_of_week 
+					ON 
+						alarm_schedule.day_of_week=day_of_week.dow_id 
+					WHERE 
+						alarm_schedule.clusterid=".$_GET['clusterid'].
+					" ORDER BY day_of_week.dow_id ASC, alarm_schedule.activate_time ASC";
 				$result=mysql_query($sql);
-				$row=mysql_fetch_array($result);
+				//$row=mysql_fetch_array($result);
 
 				$countAlarmSched = 0;
 				while($row=mysql_fetch_array($result))
